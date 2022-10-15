49e484ca88fade9d6177018e8b41704a07653898
LevBayanov <50771555+levbayanov@users.noreply.github.com>
2022-10-15 08:14:35 +0300

    Add files via upload

---
+++ TelegramBot1.0/src/Main.java
@@ -0,0 +1,19 @@
+import java.nio.file.FileSystem;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String TocenBot = "5680682119:AAEoxcQKFRBaZOOhgp_uLxn6z3Eb0aQAu94";
+
+        Bot Bot = new Bot();
+        //Scanner scanner1 = new Scanner(System.in);
+
+           // Bot.BotStartTelegram();
+            Bot.BotStartConsole();
+
+
+    }
+}
