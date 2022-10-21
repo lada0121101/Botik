266e925175882db68e13f592fbe883ab08580cc7
LevBayanov <50771555+levbayanov@users.noreply.github.com>
2022-10-21 20:40:40 +0300

    Update Main.java

--- TelegramBot1.0/src/Main.java
+++ TelegramBot1.0/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String TocenBot = "5680682119:AAEoxcQKFRBaZOOhgp_uLxn6z3Eb0aQAu94";
+        
         UserRepository userRepository = new UserRepository();
         WeatherService weatherService = new WeatherService();
         ConsoleBot Bot = new ConsoleBot(userRepository, weatherService);
