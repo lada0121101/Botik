a1d48a42b5632898aee8be26eb88dfc09690cb26
lada0121101 <82163558+lada0121101@users.noreply.github.com>
2024-02-01 13:06:42 +0500

    Добавлена реакция на команды /start,  /help, неверное написание города

--- weatherBot2/src/main/java/org/example/BotLogics.java
+++ weatherBot2/src/main/java/org/example/BotLogics.java
@@ -1,14 +1,22 @@
 package org.example;
 
 
+import org.telegram.telegrambots.meta.api.methods.groupadministration.DeleteChatStickerSet;
+
 public class BotLogics {
     WeatherService weatherService;
-    public BotLogics( WeatherService weatherService) {
+
+    public BotLogics(WeatherService weatherService) {
         this.weatherService = weatherService;
     }
 
-    public String botResponse(String textMsg, String chatId)
-    {
+    public String botResponse(String textMsg, String chatId) {
+        String start = "Приветствую, напишите название города на английском";
+        String help = "Напишите название города на английском и бот пришлëт вам инфаааацию о погоде в этом городе.\nНапример, напишите Moscow, чтобы получить информацию о температуре, кратком описании погоды, скорости ветра в Москве";
+        if (textMsg.equals("/start"))
+            return start;
+        else if (textMsg .equals("/help"))
+            return help;
         String cityWeather = weatherService.getWeather(textMsg);
         return cityWeather;
     }
