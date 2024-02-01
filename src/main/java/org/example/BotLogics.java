2a5652b8e01371d6dc780401af5b9d2950514ce8
lada0121101 <82163558+lada0121101@users.noreply.github.com>
2024-02-01 15:45:01 +0500

    Добавлен JavaDoc

--- src/main/java/org/example/BotLogics.java
+++ src/main/java/org/example/BotLogics.java
@@ -1,18 +1,29 @@
 package org.example;
 
 
-import org.telegram.telegrambots.meta.api.methods.groupadministration.DeleteChatStickerSet;
-
+/**
+ *Класс логики бота, обрабатывает сообщение пользователя и возвращает ответ бота;
+ * @author Лев Баянов, Шипиловских Лада
+ */
 public class BotLogics {
+    /**
+     * сервис для получения инфрмации о погоде в городе
+     */
     WeatherService weatherService;
 
     public BotLogics(WeatherService weatherService) {
         this.weatherService = weatherService;
     }
 
+    /**
+     * Обрабатывает сообщение пользователя и возвращет ответ в зависимости от того, обычное ли сообщение, или команда старта или помощи
+     * @param textMsg сообщение пользователя
+     * @param chatId ID чата с пользователем
+     * @return текст ответа пользователю
+     */
     public String botResponse(String textMsg, String chatId) {
         String start = "Приветствую, напишите название города на английском";
-        String help = "Напишите название города на английском и бот пришлëт вам инфаааацию о погоде в этом городе.\nНапример, напишите Moscow, чтобы получить информацию о температуре, кратком описании погоды, скорости ветра в Москве";
+        String help = "Напишите название города на английском и бот пришлëт вам информацию о погоде в этом городе.\nНапример, напишите Moscow, чтобы получить информацию о температуре, кратком описании погоды, скорости ветра в Москве";
         if (textMsg.equals("/start"))
             return start;
         else if (textMsg .equals("/help"))
@@ -21,6 +32,3 @@ public class BotLogics {
         return cityWeather;
     }
 }
-
-
-
